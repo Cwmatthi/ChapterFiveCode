a5f87c1caefbce1f7dcb4c0df03f7579adaa8cda
Cwmatthi <Codymatthias@gmail.com>
2018-03-27 21:15:14 -0400

    Tutorial 5-9 Added

---
+++ ChapterFiveCode/Tutorial 5-9/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Tutorial_5_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(64, 122);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(85, 36);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The Countries of South America";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.Location = new System.Drawing.Point(22, 34);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(190, 82);
+            this.countriesListBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 165);
+            this.Controls.Add(this.countriesListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Name = "Form1";
+            this.Text = "South America";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox countriesListBox;
+    }
+}
+
---
+++ ChapterFiveCode/Tutorial 5-9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tutorial_5_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string countryname;
+
+                StreamReader inputfile;
+
+                inputfile = File.OpenText("The Countries.txt.");
+
+                while (!inputfile.EndOfStream)
+                {
+                    countryname = inputfile.ReadLine();
+
+                    countriesListBox.Items.Add(countryname);
+                }
+
+                inputfile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
